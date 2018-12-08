2dbbb0b6262af59e3705741b2a2899fe5dfd020c
Workerlv <arturs280@inbox.lv>
2018-12-08 23:01:08 +0200

    Initial Commit

---
+++ Getting API data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Getting API data
+//
+//  Created by Arturs Vitins on 08/12/2018.
+//  Copyright © 2018 Arturs Vitins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
